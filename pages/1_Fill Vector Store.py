50138d2c930656c05b7bb42b3b0eddc9a2fdf8db
Patrick Reichmann <patrick.reichmann@codecentric.de>
2024-04-09 16:34:27 +0200

    add second page to upload files for indexing

---
+++ pages/1_Fill Vector Store.py
@@ -0,0 +1,23 @@
+from io import StringIO
+
+import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+from database.database import Doc, add_to_database
+
+uploaded_files: list[UploadedFile] = st.file_uploader(
+    "choose a file", accept_multiple_files=True, type="html"
+)
+
+if st.button("Upload", type="primary"):
+    docs: list[Doc] = []
+    for uploaded_file in uploaded_files:
+        bytes_read = uploaded_file.getvalue()
+        string_io = StringIO(bytes_read.decode("utf-8"))
+        text = string_io.read()
+        docs.append(
+            Doc(content=text, file_name=uploaded_file.name, type=uploaded_file.type)
+        )
+
+    with st.spinner():
+        add_to_database(docs)
