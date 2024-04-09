c2ee4d5f929855c746c17f6a19ab4467ad5f1d2e
Patrick Reichmann <patrick.reichmann@codecentric.de>
2024-04-09 16:35:18 +0200

    alter call stmt to work with new control flow & fix chunking error

--- database/database.py
+++ database/database.py
@@ -1,15 +1,37 @@
 import os
+from typing import TypedDict
+
 import nltk
+from bs4 import BeautifulSoup
 
 from langchain_community.document_loaders.directory import DirectoryLoader
 from langchain_community.document_loaders.html import UnstructuredHTMLLoader
 from langchain_community.document_loaders.html_bs import BSHTMLLoader
 from langchain_community.embeddings.azure_openai import AzureOpenAIEmbeddings
 from langchain_community.vectorstores.azuresearch import AzureSearch
+from langchain_core.documents import Document
 from langchain_text_splitters import CharacterTextSplitter
 
 
-def add_to_database(src_directory: str):
+class Doc:
+    content: str
+    file_name: str
+    type: str
+
+    def __init__(self, content: str, file_name: str, type: str):
+        self.content = content
+        self.file_name = file_name
+        self.type = type
+
+    def to_document(self) -> Document:
+        soup = BeautifulSoup(self.content)
+        return Document(
+            page_content=soup.get_text(),
+            metadata={"file_name": self.file_name, "type": self.type},
+        )
+
+
+def add_to_database(src_docs: list[Doc]):
     api_key = os.environ["AZURE_OPENAI_API_KEY"]
     azure_endpoint = os.environ["AZURE_OPENAI_ENDPOINT_NAME"]
     api_version = os.environ["AZURE_OPENAI_API_VERSION"]
@@ -30,7 +52,7 @@ def add_to_database(src_directory: str):
         api_key=api_key,
     )
 
-    index_name = "langchain-rag-demo"
+    index_name = "rag-prototype-streamlit"
 
     vector_store = AzureSearch(
         azure_search_endpoint=search_service_endpoint,
@@ -40,10 +62,22 @@ def add_to_database(src_directory: str):
         search_service_api_version=search_service_api_version,
     )
 
-    loader = DirectoryLoader(src_directory, glob="**/*.html", loader_cls=BSHTMLLoader)
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    # loader = DirectoryLoader(src_directory, glob="**/*.html", loader_cls=BSHTMLLoader)
+    from langchain_core.documents import Document
 
-    docs = loader.load()
+    text_splitter = CharacterTextSplitter(
+        separator=" ",
+        chunk_size=1000,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
+    )
+
+    # custom Document loading from Memory
+    docs: list[Document] = []
+    for doc in src_docs:
+        docs.append(doc.to_document())
+
     docs = text_splitter.split_documents(docs)
 
     vector_store.add_documents(documents=docs)
