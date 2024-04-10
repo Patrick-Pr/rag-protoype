46db72ae1e3b9291c57c9f8ccafcb4aec41fd025
Patrick Reichmann <patrick.reichmann@codecentric.de>
2024-04-10 17:08:04 +0200

    add display for retrieved Docs

--- chat_with_Rag.py
+++ chat_with_Rag.py
@@ -1,6 +1,9 @@
+import json
+
 import typer
 from dotenv import load_dotenv
 import streamlit as st
+from langchain_core.documents import Document
 from langchain_core.prompts import PromptTemplate
 
 from database.database import add_to_database
@@ -8,22 +11,8 @@ from search.search import retrieve_doc, ask_question
 
 load_dotenv()
 
-# app = typer.Typer()
-#
-#
-# @app.command()
-# def add(src_directory: str):
-#     add_to_database(src_directory)
-#
-#
-# @app.command()
-# def search(promt: st):
-#     res = ask_question(promt)
-#     print(res)
 
-
-def call_llm(input_str: str) -> str:
-    # TODO: do the LLM call here
+def call_llm(input_str: str) -> tuple[str, list[Document]]:
     docs = retrieve_doc(input_str)
 
     prompt = "Context:\n{context}\n\nQuestion:\n{input}"
@@ -31,12 +20,12 @@ def call_llm(input_str: str) -> str:
         input_variables=["input", "context"], template=prompt
     )
 
-    return ask_question(prompt_template, input_str, docs)
-    # return prompt
+    response = ask_question(prompt_template, input_str, docs)
+
+    return response, docs
 
 
 def main():
-
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
@@ -48,12 +37,30 @@ def main():
         st.chat_message("user").markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-        response = call_llm(prompt)
+        with st.spinner(text="Generating Answer"):
+            response, docs = call_llm(prompt)
+
         with st.chat_message("assistant"):
             st.markdown(response)
+            st.sidebar.header("Used Context")
+            for idx, document in enumerate(docs):
+                st.sidebar.subheader(
+                    f"Chunk from Document {document.metadata['file_name']}"
+                )
+                st.sidebar.write(document.page_content)
+                st.sidebar.code(
+                    json.dumps(
+                        document.metadata,
+                        sort_keys=True,
+                        indent=4,
+                        separators=(":", ","),
+                    ),
+                    language="json",
+                )
+                st.sidebar.caption("Metadata")
+
         st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 if __name__ == "__main__":
-    # app()
     main()
